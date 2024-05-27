b88e4d3c8bcf722fa3f6a9840a2a040a4d374138
alcoccoque <aleksandr.lytvynov@gmail.com>
2024-05-27 23:49:05 +0300

    fix envs

--- app/db/database.py
+++ app/db/database.py
@@ -9,10 +9,10 @@ from app.config import prod_db_settings
 print("TEST")
 print("POSTGRES_USER=", os.getenv("POSTGRES_USER"))
 print("POSTGRES_PASSWORD=", os.getenv("POSTGRES_PASSWORD"))
-print("POSTGRES_DB=", os.getenv("POSTGRES_DB"))
-print("POSTGRES_PORT=", os.getenv("POSTGRES_PORT"))
 print("POSTGRES_HOST=", os.getenv("POSTGRES_HOST"))
-print("DATABASE_URL=", prod_db_settings.DATABASE_URL)
+print("POSTGRES_PORT=", os.getenv("POSTGRES_PORT"))
+print("POSTGRES_DB=", os.getenv("POSTGRES_DB"))
+print("DATABASE_URL=", f"postgresql+asyncpg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@test-{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}")
 engine = create_async_engine(prod_db_settings.DATABASE_URL)
 async_session_maker = async_sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
